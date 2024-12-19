06325e6b8d1efbc45412352b227b094c16c5d53b
Hugo Torrico <torricomarquezh@gmail.com>
2024-12-18 22:52:17 -0500

    windows forms

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Services;
+
+Console.WriteLine("Hello, World!");
+
+var service = new CourseService();
+var servicios = service.Get();
+
+foreach (var item in servicios)
+{
+    Console.WriteLine(item.Name);
+}
+
+Console.Read();
+
